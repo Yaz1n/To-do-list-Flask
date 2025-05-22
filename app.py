81e757a0195a3ed7df3843ad17166eda15c377b7
Mohmed Yazin <118006811+Yaz1n@users.noreply.github.com>
2025-05-23 00:09:07 +0530

    added date and better gui

--- app.py
+++ app.py
@@ -1,30 +1,64 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
+
+
 
 app = Flask(__name__)
-todo_list = []
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+# Task model (table)
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+    due_date = db.Column(db.Date, nullable=True)
+
+# Home route
 @app.route('/')
 def index():
-    return render_template('index.html', tasks=todo_list)
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
 
+# Add task
 @app.route('/add', methods=['POST'])
 def add():
-    task = request.form.get('task')
-    if task:
-        todo_list.append({'task': task, 'done': False})
+    task_content = request.form.get('task')
+    due_date_str = request.form.get('due_date')
+    
+    due_date = None
+    if due_date_str:
+        due_date = datetime.strptime(due_date_str, '%Y-%m-%d').date()
+
+    if task_content:
+        new_task = Task(content=task_content, due_date=due_date)
+        db.session.add(new_task)
+        db.session.commit()
     return redirect(url_for('index'))
 
+# Mark task as done
 @app.route('/complete/<int:task_id>')
 def complete(task_id):
-    if 0 <= task_id < len(todo_list):
-        todo_list[task_id]['done'] = True
+    task = Task.query.get_or_404(task_id)
+    task.done = True
+    db.session.commit()
     return redirect(url_for('index'))
 
+# Delete task
 @app.route('/delete/<int:task_id>')
 def delete(task_id):
-    if 0 <= task_id < len(todo_list):
-        todo_list.pop(task_id)
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create tables on first run
     app.run(debug=True)
